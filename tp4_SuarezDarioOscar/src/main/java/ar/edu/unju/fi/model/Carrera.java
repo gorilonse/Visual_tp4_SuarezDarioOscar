7369ddcf08f4425ffabb1381196efed30541be7c
gorilonse <suarez_dos@hotmail.com>
2024-05-25 23:25:58 -0300

    section-1 + navMenu

---
+++ tp4_SuarezDarioOscar/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -0,0 +1,46 @@
+package ar.edu.unju.fi.model;
+
+public class Carrera {
+
+	//ATRIBUTOS
+	private String codigo;
+	private String nombre;
+	private String cantidadDeAnios;
+	private String estado;
+	
+	
+	
+	
+	
+	
+	//GETTER Y SETTER
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCantidadDeAnios() {
+		return cantidadDeAnios;
+	}
+	public void setCantidadDeAnios(String cantidadDeAnios) {
+		this.cantidadDeAnios = cantidadDeAnios;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+	 
+	
+}
---
+++ tp4_SuarezDarioOscar/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -0,0 +1,49 @@
+package ar.edu.unju.fi.model;
+
+public class Docente {
+	//ATRIBUTO
+	private String legajo;
+	private String nombre;
+	private String epellido;
+	private String email;
+	private String telefono;
+	
+	
+	
+	//GETTER Y SETTER 
+	public String getLegajo() {
+		return legajo;
+	}
+	public void setLegajo(String legajo) {
+		this.legajo = legajo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEpellido() {
+		return epellido;
+	}
+	public void setEpellido(String epellido) {
+		this.epellido = epellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	
+	
+
+}
